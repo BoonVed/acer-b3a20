13a966d26e24ae7c981e93f14e892b2eb6cdf3ae
noob dev <76555519+BoonVed@users.noreply.github.com>
2021-09-22 18:33:48 +0000

    initial commit

---
+++ omni_B3-A20.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2015 The Android Open Source Project
+# Copyright (C) 2015 The TWRP Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit 64bit support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := B3-A20
+PRODUCT_NAME := omni_B3-A20
+PRODUCT_BRAND := acer
+PRODUCT_MODEL := Acer Iconia one B3-A20
+PRODUCT_MANUFACTURER := acer
+PRODUCT_RELEASE_NAME := Acer Iconia one B3-A20
